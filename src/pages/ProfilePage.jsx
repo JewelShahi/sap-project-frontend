4936067b08f82fd396f96306c89851eb9dea4ba3
JoeTheBest <jewelshahi10@gmail.com>
2026-03-21 00:42:53 +0200

    fix: adjust indentation and formatting in ProfilePage component

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -17,78 +17,78 @@ const ProfilePage = () => {
       <Animate variant="fade-down">
         <div className="card bg-base-200 p-6 flex items-center gap-6">
 
-            <div className="avatar placeholder">
-    <div className="w-20 rounded-full bg-primary text-primary-content">
-      <span className="text-xl text-xs font-bold">MP</span>
-    </div>
-      </div>
+          <div className="avatar placeholder">
+            <div className="w-20 rounded-full bg-primary text-primary-content">
+              <span className="text-xl text-xs font-bold">MP</span>
+            </div>
+          </div>
 
-    <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center gap-2">
 
-  <div className="flex flex-col items-center gap-2">
-  <div className="flex items-center gap-2">
-    <span className="text-xl font-bold">Name Name</span>
-    <span className="indicator-item badge badge-success badge-xs"></span>
-  </div>
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex items-center gap-2">
+                <span className="text-xl font-bold">Name Name</span>
+                <span className="indicator-item badge badge-success badge-xs"></span>
+              </div>
 
-  <span className="badge badge-primary">Admin</span>
-</div>
+              <span className="badge badge-primary">Admin</span>
+            </div>
 
-  {/* Email */}
-  <span className="text-sm text-base-content/60">
-    email@example.com
-  </span>
+            {/* Email */}
+            <span className="text-sm text-base-content/60">
+              email@example.com
+            </span>
 
-  {/* Created on */}
-  <span className="text-sm text-base-content/40">
-    Created on: Jan 12, 2024
-  </span>
+            {/* Created on */}
+            <span className="text-sm text-base-content/40">
+              Created on: Jan 12, 2024
+            </span>
 
-{/* Edit Profile */}
-  <button
-  className="btn btn-sm btn-outline mt-2"
-  onClick={() => document.getElementById('edit_profile_modal').showModal()}
->
-  Edit Profile
-</button>
+            {/* Edit Profile */}
+            <button
+              className="btn btn-sm btn-outline mt-2"
+              onClick={() => document.getElementById('edit_profile_modal').showModal()}
+            >
+              Edit Profile
+            </button>
 
-{/* Sign Out */}
-<NavLink to ="/login"
-  className="btn btn-sm btn-outline mt-2"
-  // onClick={() => document.getElementById('edit_profile_modal').showModal()}
->
-  Sign Out
-</NavLink>
+            {/* Sign Out */}
+            <NavLink to="/login"
+              className="btn btn-sm btn-outline mt-2"
+            // onClick={() => document.getElementById('edit_profile_modal').showModal()}
+            >
+              Sign Out
+            </NavLink>
 
-</div>
-<dialog id="edit_profile_modal" className="modal">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg mb-4">Edit Profile</h3>
+          </div>
+          <dialog id="edit_profile_modal" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg mb-4">Edit Profile</h3>
 
-    {/* Name */}
-    <label className="label text-xs">Name</label>
-    <input
-      type="text"
-      placeholder="New name"
-      className="input input-bordered w-full mb-3"
-    />
+              {/* Name */}
+              <label className="label text-xs">Name</label>
+              <input
+                type="text"
+                placeholder="New name"
+                className="input input-bordered w-full mb-3"
+              />
 
-    {/* Password */}
-    <label className="label text-xs">Password</label>
-    <input
-      type="password"
-      placeholder="New password"
-      className="input input-bordered w-full mb-4"
-    />
+              {/* Password */}
+              <label className="label text-xs">Password</label>
+              <input
+                type="password"
+                placeholder="New password"
+                className="input input-bordered w-full mb-4"
+              />
 
-    <div className="modal-action">
-      <form method="dialog">
-        <button className="btn btn-ghost">Cancel</button>
-        <button className="btn btn-primary">Save</button>
-      </form>
-    </div>
-  </div>
-</dialog>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button className="btn btn-ghost">Cancel</button>
+                  <button className="btn btn-primary">Save</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
 
 
         </div>
@@ -171,8 +171,8 @@ const ProfilePage = () => {
 
           <div className="space-y-3">
             <Animate>
-        <RepoCard repos={REPOS} />
-      </Animate>
+              <RepoCard repos={REPOS} />
+            </Animate>
           </div>
         </div>
       </Animate>
